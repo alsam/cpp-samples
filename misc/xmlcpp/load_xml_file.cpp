4bac4e0c3ca008f4b8bc67927d1ef3ebfef3b2b6
Alexander Samoilov <alexander.samoilov@gmail.com>
2016-09-17 15:30:24 +0300

    use case for xml load with c++.

---
+++ misc/xmlcpp/load_xml_file.cpp
@@ -0,0 +1,17 @@
+#include "pugixml.hpp"
+
+#include <iostream>
+
+int main()
+{
+// tag::code[]
+    pugi::xml_document doc;
+
+    pugi::xml_parse_result result = doc.load_file("maven.xml");
+
+    std::cout << "Load result: " << result.description() << "\n";
+    std::cout << "version name: " << doc.child("metadata").child("version").child_value() << std::endl;
+    std::cout << "lastUpdated: " << doc.child("metadata").child("versioning").child("lastUpdated").child_value() << std::endl;
+// end::code[]
+}
+
