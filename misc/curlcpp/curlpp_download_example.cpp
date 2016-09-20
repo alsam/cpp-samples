c5781d2f81f656b718db95321a4a03920b2781fd
Alexander Samoilov <alexander.samoilov@gmail.com>
2016-09-20 19:46:10 +0300

    expand on curlpp example.

--- misc/curlcpp/curlpp_download_example.cpp
+++ misc/curlcpp/curlpp_download_example.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
@@ -78,9 +79,9 @@ struct simple_walker: pugi::xml_tree_walker {
 };
 
 void
-pull_one_url(std::string const& url, bool maven)
+getWebPage(std::string const& url, std::string& page)
 {
-    //std::unique_lock<std::mutex> lk(m);
+    std::unique_lock<std::mutex> lk(m);
     try {
         // That's all that is needed to do cleanup of used resources (RAII style).
         curlpp::Cleanup myCleanup;
@@ -90,7 +91,6 @@ pull_one_url(std::string const& url, bool maven)
 
         // Set the URL.
         myRequest.setOpt(new curlpp::options::Url(url));
-	//myRequest.setOpt(new curlpp::options::FtpListOnly(true));
 
         // Send request and get a result.
         // By default the result goes to standard output.
@@ -99,7 +99,85 @@ pull_one_url(std::string const& url, bool maven)
         curlpp::options::WriteStream ws(&os);
         myRequest.setOpt(ws);
         myRequest.perform();
-	std::cout << "os: " << cleanhtml(os.str()) << std::endl;
+
+        page = std::move(os.str());
+
+        is_ready = true;
+        cv.notify_one();
+
+    } catch(curlpp::RuntimeError & e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    catch(curlpp::LogicError & e) {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+std::vector<std::string>
+dirList(std::string const& html)
+{
+    std::string cleaned_xml{cleanhtml(html)};
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_string(cleaned_xml.c_str());
+    std::cout << "html.body.h1: " << doc.child("html").child("body").child("h1").child_value() << std::endl;
+    auto table = doc.child("html").child("body").child("table");
+    // tag::code[]
+    for (auto it = table.begin(); it != table.end(); ++it) {
+      std::cout << "TR;";
+      auto td = it->child("td").child("a");
+      for (pugi::xml_attribute_iterator ait = td.attributes_begin(); ait != td.attributes_end(); ++ait) {
+          std::cout << " " << ait->name() << " <=> " << ait->value();
+      }
+      std::cout << " TD " << td << std::endl;
+    }
+
+    std::vector<std::string> ret;
+    return ret;
+}
+
+std::vector<std::string>
+parseManifest(std::string const& manifest_xml)
+{
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_string(manifest_xml.c_str());
+
+    if (result) {
+        std::cout << "XML parsed without errors\n";
+    } else {
+        std::cout << "Error description: " << result.description() << "\n";
+    }
+
+    std::cout << "version name: " << doc.child("metadata").child("version").child_value() << std::endl;
+    std::cout << "lastUpdated: " << doc.child("metadata").child("versioning").child("lastUpdated").child_value() << std::endl;
+
+    std::vector<std::string> ret;
+    return ret;
+}
+
+void
+pull_one_url(std::string const& url, bool maven)
+{
+    std::unique_lock<std::mutex> lk(m);
+    try {
+        // That's all that is needed to do cleanup of used resources (RAII style).
+        curlpp::Cleanup myCleanup;
+
+        // Our request to be sent.
+        curlpp::Easy myRequest;
+
+        // Set the URL.
+        myRequest.setOpt(new curlpp::options::Url(url));
+        //myRequest.setOpt(new curlpp::options::FtpListOnly(true));
+
+        // Send request and get a result.
+        // By default the result goes to standard output.
+
+        std::ostringstream os;
+        curlpp::options::WriteStream ws(&os);
+        myRequest.setOpt(ws);
+        myRequest.perform();
+        std::cout << "os: " << cleanhtml(os.str()) << std::endl;
 
         pugi::xml_document doc;
         pugi::xml_parse_result result = doc.load_string(cleanhtml(os.str()).c_str());
@@ -118,15 +196,15 @@ pull_one_url(std::string const& url, bool maven)
             std::cout << "lastUpdated: " << doc.child("metadata").child("versioning").child("lastUpdated").child_value() << std::endl;
         } else {
             std::cout << "html.body.h1: " << doc.child("html").child("body").child("h1").child_value() << std::endl;
-	    auto table = doc.child("html").child("body").child("table");
+            auto table = doc.child("html").child("body").child("table");
             // tag::code[]
             for (auto it = table.begin(); it != table.end(); ++it) {
               std::cout << "TR;";
-	      auto td = it->child("td").child("a");
+              auto td = it->child("td").child("a");
               for (pugi::xml_attribute_iterator ait = td.attributes_begin(); ait != td.attributes_end(); ++ait) {
                   std::cout << " " << ait->name() << " <=> " << ait->value();
               }
-	      std::cout << " TD " << td << std::endl;
+              std::cout << " TD " << td << std::endl;
             }
             // end::code[]
         }
@@ -144,16 +222,17 @@ pull_one_url(std::string const& url, bool maven)
 
 int main(int, char **)
 {
-    std::string Url1 = "https://oss.sonatype.org/content/repositories/snapshots/edu/berkeley/cs/chisel3_2.11/";
-    std::string Url2 = "https://oss.sonatype.org/content/repositories/snapshots/edu/berkeley/cs/chisel3_2.11/3.0-BETA-SNAPSHOT/maven-metadata.xml";
-    std::thread t1(pull_one_url, Url1, false);
-    //std::thread t2(pull_one_url, Url2, true);
+    std::string url1 = "https://oss.sonatype.org/content/repositories/snapshots/edu/berkeley/cs/chisel3_2.11/";
+    std::string url2 = "https://oss.sonatype.org/content/repositories/snapshots/edu/berkeley/cs/chisel3_2.11/3.0-BETA-SNAPSHOT/maven-metadata.xml";
+    std::string page;
+    //getWebPage(url1, page);
+    std::thread t1(getWebPage, url1, std::ref(page));
     std::unique_lock<std::mutex> lk(m);
     while (!is_ready) {
         cv.wait(lk);
     }
     t1.join();
-    //t2.join();
+    dirList(page);
 
     return 0;
 }
