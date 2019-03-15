e79c5034f8e834d94b04feb620234880b1c82f85
Alexander Samoilov <alexander.samoilov@gmail.com>
2019-03-15 07:09:45 +0100

    added rtp code chunks.

---
+++ rtp/rtp_client.cpp
@@ -0,0 +1,179 @@
+#include <ros/ros.h>
+
+#include <sstream>
+
+#include <rwc_msgs/RobotCommand.h>
+#include <rwc_msgs/RobotExecutorSrv.h>
+#include <rwc_msgs/RobotStatusSrv.h>
+
+#include <robot_connector/utils.h>
+#include <rtp_simulator/SimpleRobotInterface.h>
+
+using namespace std;
+namespace pt = boost::property_tree;
+
+map<string, rwc::RobotConfig> configs;
+
+SimpleRobotInterface::Responce SimpleRobotInterface::executeTask(rwc_msgs::RobotTask&& task)
+{
+  auto status = this->start(task);
+  status = this->execute(task);
+}
+
+template <typename T>
+SimpleRobotInterface::Responce SimpleRobotInterface::start(T&& task)
+{
+  std::cout << "start\n";
+}
+
+template <typename T>
+SimpleRobotInterface::Responce SimpleRobotInterface::execute(T&& task)
+{
+  std::cout << "execute\n";
+}
+
+rwc_msgs::RobotCommand toRobotCommand(const rwc::Operation operation, const rwc::RobotConfig robotConfig)
+{
+  rwc_msgs::RobotCommand cmd;
+
+  cmd.robot_name = operation.arm;
+  const auto& vals = operation.values;
+
+  if (operation.type == "joints")
+  {
+    cmd.operation = rwc_msgs::RobotCommand::JOINTS;
+
+    cmd.joint_trajectory.joint_names = robotConfig.joints;
+
+    cmd.joint_trajectory.points.resize(1);
+    auto &traj = cmd.joint_trajectory.points[0];
+    // cannot apply `operator=` directly due to `std::vector<float>` vs. `std::vector<double>` types mismatch
+    std::copy(vals.begin(), vals.end(), std::inserter(traj.positions, traj.positions.end()));
+    traj.velocities = { 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 };
+  }
+  else if (operation.type == "linear")
+  {
+    cmd.operation = rwc_msgs::RobotCommand::LINEAR;
+  }
+  else if (operation.type == "weld")
+  {
+    cmd.operation = rwc_msgs::RobotCommand::WELD_LINEAR;
+  }
+  else if (operation.type == "attach")
+  {
+    cmd.operation = rwc_msgs::RobotCommand::ATTACH;
+  }
+  else if (operation.type == "detach")
+  {
+    cmd.operation = rwc_msgs::RobotCommand::DETACH;
+  }
+  else {
+    stringstream str;
+    str << "unknown operation type " << operation.type;
+    throw std::runtime_error(str.str());
+  }
+
+  switch (cmd.operation)
+  {
+    case rwc_msgs::RobotCommand::LINEAR: case rwc_msgs::RobotCommand::WELD_LINEAR:
+      {
+        rwc_msgs::EulerPose eu_pose;
+        eu_pose.x = vals[0];
+        eu_pose.y = vals[1];
+        eu_pose.z = vals[2];
+        eu_pose.w = vals[3];
+        eu_pose.p = vals[4];
+        eu_pose.r = vals[5];
+        cmd.linear_poses.emplace_back(eu_pose);
+      }
+    default:
+      break;
+  }
+
+  return cmd;
+}
+
+int main(int argc, char **argv)
+{
+  // initialize node
+  ros::init(argc, argv, "rtp_sim_client");
+
+  ros::NodeHandle nh;
+
+  string configPath;
+
+  if (ros::param::has("config"))
+  {
+    ros::param::get("config", configPath);
+  }
+  else
+  {
+    ROS_INFO("No robots config, skip connection");
+    return 0;
+  }
+
+  pt::ptree pt;
+  pt::read_json(configPath, pt);
+
+  vector<rwc::RobotConfig> cRobots = rwc::parseRobotsConfig(pt.get_child("robots"));
+  ROS_INFO("test %ld robots", cRobots.size());
+  for (rwc::RobotConfig c : cRobots)
+  {
+    configs[c.name] = c;
+  }
+
+  vector<rwc_msgs::RobotErrorHandler> error_handlers;
+  vector<rwc_msgs::RobotTask> tasks;
+
+  for (const auto &cmd_entry : pt.get_child("commands"))
+  {
+    rwc::Operation operation;
+    operation.arm = cmd_entry.second.get<string>("arm");
+    operation.type = cmd_entry.second.get<string>("type");
+    for (const auto &value : cmd_entry.second.get_child("values"))
+      operation.values.push_back(value.second.get_value<float>());
+
+    std::vector<rwc::Operation> operations = rwc::parseRobotsOperations(cmd_entry.second.get_child("operations"));
+
+    if (operation.type == "task")
+    {
+      rwc_msgs::RobotTask robotTask;
+      robotTask.task_id = (int)operation.values[0];
+      robotTask.error_id = (int)operation.values[1];
+      robotTask.robot_name = operation.arm;
+      for (const rwc::Operation &o : operations)
+      {
+        rwc_msgs::RobotCommand cmd = toRobotCommand(o, configs[o.arm]);
+        robotTask.commands.push_back(cmd);
+      }
+      tasks.push_back(robotTask);
+    }
+    else if (operation.type == "error_handler")
+    {
+      rwc_msgs::RobotErrorHandler error_handler;
+      error_handler.error_id = (int)operation.values[0];
+      error_handler.robot_name = operation.arm;
+      for (const rwc::Operation &o : operations)
+      {
+        rwc_msgs::RobotCommand cmd = toRobotCommand(o, configs[o.arm]);
+        error_handler.commands.push_back(cmd);
+      }
+      error_handlers.push_back(error_handler);
+    }
+  }
+
+  try
+  {
+    SimpleRobotInterface robot_interface(std::move(error_handlers));
+    for (auto &task : tasks)
+    {
+      robot_interface.executeTask(std::move(task));
+    }
+  } catch (const exception &e)
+  {
+    ROS_ERROR("failed execute test :: %s", e.what());
+    return 1;
+  }
+
+  return 0;
+}
---
+++ rtp/rtp_server.cpp
@@ -0,0 +1,395 @@
+#include <ros/ros.h>
+
+#include <sstream>
+
+#include <rwc_msgs/RobotCommand.h>
+#include <rwc_msgs/RobotExecutorSrv.h>
+#include <rwc_msgs/RobotStatusSrv.h>
+
+#include <robot_connector/msg_type.h>
+#include <robot_connector/utils.h>
+
+#include <industrial_msgs/RobotStatus.h>
+#include <rwc_msgs/RobotStatusSrv.h>
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <memory>
+#include <set>
+#include <deque>
+#include <utility>
+#include <boost/asio.hpp>
+#include <rtp_simulator/SimpleRobotInterface.h>
+
+using namespace boost::asio;
+namespace pt = boost::property_tree;
+
+Buffer<RobotCommand> commandQueue;
+
+class RobotService
+{
+// service type
+public:
+  enum ServiceType
+  {
+    TRAJECTORY_STREAMING, ROBOT_STATE,
+  };
+
+public:
+
+  RobotService(ServiceType service_type)
+  : service_type_(service_type)
+  {}
+
+  void StartHandling(std::shared_ptr<ip::tcp::socket> sock)
+  {
+    std::thread th([this, sock] { HandleClient(sock); });
+    th.detach();
+  }
+
+// methods
+private:
+
+  struct __attribute__((__packed__)) PacketHeader
+  {
+    int32_t msg_len;
+    int32_t msg_type;   // identifies type of message (standard and robot specific values)
+    int32_t comm_type;
+    int32_t reply_code; // only valid in service replies
+  };
+
+  template <typename T>
+  PacketHeader makeHeader(rwc::MsgType msg_type)
+  {
+    namespace sm = industrial::simple_message;
+    auto reply_header = PacketHeader { msg_len : sizeof(T)
+                                               + sizeof(PacketHeader)
+                                               - sizeof(int32_t),
+                                       msg_type : toUType(msg_type),
+                                       comm_type : sm::CommType::SERVICE_REPLY, // i.e. 3
+                                       reply_code : sm::ReplyType::SUCCESS // i.e. 1
+                                     };
+    return reply_header;
+  }
+
+  struct __attribute__((__packed__)) TaskMessage
+  {
+    int32_t task_id;
+    int32_t error_handler;
+    int32_t count;
+  };
+
+  constexpr static uint32_t MAX_JOINTS = 10;
+  struct __attribute__((__packed__)) JointResponceMessage // 44 Bytes
+  {
+    int32_t sequence_id;
+    float joints[MAX_JOINTS];
+
+    JointResponceMessage(int32_t id)
+      : sequence_id(id), joints{0.f,}
+    {}
+  };
+
+  struct __attribute__((__packed__)) JointTrajPtMessage
+    : public JointResponceMessage // 56 Bytes
+  {
+    float velocity;
+    float duration;
+
+    JointTrajPtMessage(int32_t id = 0)
+      : JointResponceMessage(id)
+    {}
+  };
+
+  struct __attribute__((__packed__)) TaskResult
+  {
+    int32_t task_id;
+    int32_t sequence_id;
+    int32_t error_code;
+  };
+
+  PacketHeader readHeader(std::istream &is)
+  {
+    PacketHeader header;
+    is.read((char*)&header, sizeof(PacketHeader));
+    return header;
+  }
+
+  void readBody(int32_t msg_type, std::istream &is)
+  {
+    if (msg_type == toUType(rwc::MsgType::TASK))
+    {
+      TaskMessage task_message;
+      is.read((char*)&task_message, sizeof(TaskMessage));
+      std::cout << "task_id: " << task_message.task_id
+                << " error_handler: " << task_message.error_handler
+                << " count: " << task_message.count
+                << std::endl;
+
+      current_task_id_.store(task_message.task_id);
+      commandQueue.add(RobotCommand { task_id : task_message.task_id,
+                                      sequence_id : -1,
+                                      command_type : 0 } );
+    }
+    else if (msg_type == toUType(rwc::MsgType::JOINT_TRAJ_PT))
+    {
+      JointTrajPtMessage joint_traj_pt_msg;
+      industrial::joint_data::JointData joint_data;
+      is.read((char*)&joint_traj_pt_msg, sizeof(JointTrajPtMessage));
+      std::cout << "joint_traj_pt_msg: sequence_id: " << joint_traj_pt_msg.sequence_id
+                << " velocity: " << joint_traj_pt_msg.velocity << "\n joints : [ ";
+      for (size_t i = 0; i < MAX_JOINTS; ++i)
+      {
+        std::cout << joint_traj_pt_msg.joints[i] << " ";
+      }
+      std::cout << "]\n";
+
+      commandQueue.add(RobotCommand { task_id : current_task_id_.load(),
+                                      sequence_id : joint_traj_pt_msg.sequence_id,
+                                      command_type : 1 } );
+
+    }
+  }
+
+  void readPacket(std::shared_ptr<ip::tcp::socket> sock)
+  {
+    boost::asio::streambuf b;
+
+    // reserve 512 bytes in output sequence
+    auto bufs = b.prepare(512);
+    size_t n = sock->receive(bufs);
+    std::cout << "read packed n: " << n << " bytes\n";
+
+    // received data is "committed" from output sequence to input sequence
+    b.commit(n);
+
+    std::istream is(&b);
+
+    PacketHeader header = readHeader(is);
+    std::cout
+      << "msg_len: " << header.msg_len
+      << " msg_type: " << rwc::MsgName(header.msg_type) << "(" << header.msg_type << ")"
+      << " comm_type: " << header.comm_type
+      << " reply_code: " << header.reply_code << std::endl;
+
+    readBody(header.msg_type, is);
+  }
+
+  /// the main workforce for communicating with robot-connector
+  void HandleClient(std::shared_ptr<ip::tcp::socket> sock)
+  {
+    try
+    {
+      if (service_type_ == TRAJECTORY_STREAMING)
+      {
+        for (;;)
+        {
+          readPacket(sock);
+
+          auto reply_header = makeHeader<JointResponceMessage>(rwc::MsgType::JOINT_RESPONCE);
+          boost::asio::write(*sock.get(), boost::asio::buffer((void*)&reply_header, sizeof(reply_header)));
+
+          JointResponceMessage reply(1);
+          boost::asio::write(*sock.get(), boost::asio::buffer((void*)&reply, sizeof(reply)));
+        }
+      }
+      else if (service_type_ == ROBOT_STATE)
+      {
+        using namespace std::chrono_literals;
+        for (;;)
+        {
+          constexpr size_t status_freq = 10;
+          for (size_t i = 0; i < status_freq; ++i)
+          {
+            industrial_msgs::RobotStatus status;
+            auto reply_header = makeHeader<decltype(status)>(rwc::MsgType::STATUS);
+            boost::asio::write(*sock.get(), boost::asio::buffer((void*)&reply_header, sizeof(reply_header)));
+            boost::asio::write(*sock.get(), boost::asio::buffer((void*)&status, sizeof(status)));
+          }
+
+          if (!commandQueue.empty())
+          {
+            RobotCommand cmd = commandQueue.remove();
+            if (cmd.command_type != 0) // TODO for task command, enum instead of constant
+            {
+              auto task_result = TaskResult { task_id : cmd.task_id, sequence_id : /* cmd.sequence_id */ 0, error_code : 0 };
+              //auto task_result = TaskResult { task_id : current_task_id_, sequence_id : /* cmd.sequence_id */ 0, error_code : 0 };
+              auto task_reply_header = makeHeader<decltype(task_result)>(rwc::MsgType::TASK_RESULT);
+              boost::asio::write(*sock.get(), boost::asio::buffer((void*)&task_reply_header, sizeof(task_reply_header)));
+              boost::asio::write(*sock.get(), boost::asio::buffer((void*)&task_result, sizeof(task_result)));
+            }
+          }
+          //std::this_thread::sleep_for(200ms);
+        }
+      }
+    }
+    catch (boost::system::system_error &e)
+    {
+      std::cout << "Error occured! Error code = "
+                << e.code() << ". Message: "
+                << e.what();
+    }
+
+    onFinish();
+  }
+
+  /// cleanup.
+  void onFinish() {
+    delete this;
+  }
+
+// data members
+private:
+
+  std::shared_ptr<ip::tcp::socket> sock_;
+
+  ServiceType service_type_;
+
+  std::atomic<int32_t> current_task_id_;
+};
+
+class RobotAcceptor {
+public:
+
+  RobotAcceptor(io_service &ios, uint16_t port_num)
+    : ios_(ios),
+      acceptor_(ios_,
+                ip::tcp::endpoint(ip::address_v4::any(),
+                                  port_num))
+  {
+    acceptor_.listen();
+  }
+
+  void Accept(RobotService::ServiceType service_type)
+  {
+    auto sock = std::make_shared<ip::tcp::socket>(ios_);
+    acceptor_.accept(*sock.get());
+    (new RobotService(service_type))->StartHandling(sock);
+  }
+
+// methods
+private:
+
+// data members
+private:
+
+  io_service &ios_;
+  ip::tcp::acceptor acceptor_;
+};
+
+class RobotSimulatorServer
+{
+public:
+
+  RobotSimulatorServer()
+    : stop_(false) {}
+
+  void Start(uint16_t port_num, RobotService::ServiceType service_type)
+  {
+    thread_.reset(new std::thread(
+          [this, port_num, service_type]
+          {
+            this->Run(port_num, service_type);
+          })
+        );
+  }
+
+  void Stop()
+  {
+    stop_.store(true);
+    thread_->join();
+  }
+
+// methods
+private:
+  void Run(uint16_t port_num, RobotService::ServiceType service_type)
+  {
+    RobotAcceptor acc(ios_, port_num);
+
+    while (!stop_.load())
+    {
+      acc.Accept(service_type);
+    }
+  }
+
+// data members
+private:
+
+  std::unique_ptr<std::thread> thread_;
+  std::atomic<bool> stop_;
+  io_service ios_;
+
+};
+
+int main(int argc, char** argv)
+{
+
+  // initialize node
+  ros::init(argc, argv, "rtp_simulator");
+
+  std::string configPath;
+
+  if (ros::param::has("config"))
+  {
+    ros::param::get("config", configPath);
+  }
+  else
+  {
+    ROS_INFO("No robots config, skip connection");
+    return 0;
+  }
+
+  pt::ptree pt;
+  pt::read_json(configPath, pt);
+
+  try
+  {
+    std::vector<rwc::RobotConfig> cRobots = rwc::parseRobotsConfig(pt.get_child("robots"));
+    ROS_INFO("succesfully parsed %zu robots", cRobots.size());
+
+    for (rwc::RobotConfig &conf: cRobots)
+    {
+      if (!conf.connect)
+      {
+        ROS_INFO("skip %s::%s robot", conf.name.c_str(), conf.ip.c_str());
+        continue;
+      }
+
+      std::cout << "establishing TCP connection to ip: " << conf.ip
+                << " state port: " << conf.state_port
+                << " joints ports: " << conf.joints_port << std::endl;
+
+      // creating two servers per robot: joints and state
+      try
+      {
+        RobotSimulatorServer* srv = new RobotSimulatorServer[2];
+        ROS_INFO("starting joints server");
+        srv[0].Start(conf.joints_port, RobotService::TRAJECTORY_STREAMING);
+        ROS_INFO("starting state server");
+        srv[1].Start(conf.state_port, RobotService::ROBOT_STATE);
+
+      }
+      catch (boost::system::system_error &e)
+      {
+        std::cout << "Error occured! Error code = "
+                  << e.code() << ". Message: "
+                  << e.what();
+      }
+    }
+
+    ros::waitForShutdown();
+
+  }
+  catch (const std::exception &e)
+  {
+    ROS_INFO("err parse config : %s", e.what());
+  }
+
+
+  return 0;
+}
+
