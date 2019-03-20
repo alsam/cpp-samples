54f9b7bb9746b706be26c23123593162b2ed467e
alsam <alexander.samoilov@gmail.com>
2019-03-20 16:21:28 +0300

    added unit tests to specialization sample

--- c++new-features/spec.cpp
+++ c++new-features/spec.cpp
@@ -36,7 +36,7 @@ struct Inserter
     OutputIterator operator()(Type& cont) { return cont.begin(); }
     size_t dist(const Type& cont, OutputIterator it)
     {
-        return std::distance(cont.begin(), it);
+        return std::distance<typename T::const_iterator>(cont.begin(), it);
     }
 };
 
@@ -62,8 +62,7 @@ typename Inserter<T>::OutputIterator append(T& cont)
 template <typename T>
 size_t dist(const T& cont, typename Inserter<T>::OutputIterator it)
 {
-    Inserter<T> inserter;
-    return inserter.dist(cont, it);
+    return Inserter<T>().dist(cont, it);
 }
 
 template <typename RetCont, typename AIterator, typename BIterator, typename SetOp>
@@ -72,14 +71,14 @@ int setOpIndices(RetCont& result,
                  BIterator contBbegin, BIterator contBend,
                  SetOp setOp)
 {
-  RetCont contAcopy, contBcopy;
-  std::copy(contAbegin, contAend, append(contAcopy));
-  std::copy(contBbegin, contBend, append(contBcopy));
-  boost::sort(contAcopy);
-  boost::sort(contBcopy);
-  auto initialPosition = append(result);
-  auto pos = setOp(contAcopy, contBcopy, append(result));
-  return dist(result, pos);
+    RetCont contAcopy, contBcopy;
+    std::copy(contAbegin, contAend, append(contAcopy));
+    std::copy(contBbegin, contBend, append(contBcopy));
+    boost::sort(contAcopy);
+    boost::sort(contBcopy);
+    auto initialPosition = append(result);
+    auto pos = setOp(contAcopy, contBcopy, append(result));
+    return dist(result, pos);
 }
 
 template <typename RetCont, typename AIterator, typename BIterator,
@@ -88,8 +87,8 @@ int diffIndices(RetCont& result,
                 AIterator contAbegin, AIterator contAend,
                 BIterator contBbegin, BIterator contBend)
 {
-  return setOpIndices(result, contAbegin, contAend, contBbegin, contBend,
-    boost::set_difference<RetCont, RetCont, OutputIterator>);
+    return setOpIndices(result, contAbegin, contAend, contBbegin, contBend,
+      boost::set_difference<RetCont, RetCont, OutputIterator>);
 }
 
 template <typename RetCont, typename AIterator, typename BIterator,
@@ -98,42 +97,57 @@ int intersectIndices(RetCont& result,
                      AIterator contAbegin, AIterator contAend,
                      BIterator contBbegin, BIterator contBend)
 {
-  return setOpIndices(result, contAbegin, contAend, contBbegin, contBend,
-    boost::set_intersection<RetCont, RetCont, OutputIterator>);
+    return setOpIndices(result, contAbegin, contAend, contBbegin, contBend,
+      boost::set_intersection<RetCont, RetCont, OutputIterator>);
 }
 
 // range-based syntax sugar BEWARE uses std::back_inserter 
 template <typename RetCont, typename ARange, typename BRange>
 int diffIndicesRange(RetCont& result, const ARange& acont, const BRange& bcont)
 {
-  return diffIndices<RetCont, typename ARange::const_iterator, typename BRange::const_iterator,
-                     std::back_insert_iterator<RetCont>>
-    (result, begin(acont), end(acont), begin(bcont), end(bcont));
+    result.clear();
+    return diffIndices<RetCont, typename ARange::const_iterator, typename BRange::const_iterator,
+                       std::back_insert_iterator<RetCont>>
+      (result, begin(acont), end(acont), begin(bcont), end(bcont));
 }
 
 template <typename RetCont, typename ARange, typename BRange>
 int intersectIndicesRange(RetCont& result, const ARange& acont, const BRange& bcont)
-{    
-  return intersectIndices<RetCont, typename ARange::const_iterator, typename BRange::const_iterator,
-                          std::back_insert_iterator<RetCont>>
-    (result, begin(acont), end(acont), begin(bcont), end(bcont));
-}
-
-int main()
 {
-    std::array<double, 3> arr{1,2,3}, crr{17,18,19};
-    std::vector<double> brr{7,8,9};
-    std::copy(std::begin(brr), std::end(brr), append(arr));
-    std::cout << "arr[0] : " << arr[0] << " arr[1] : " << arr[1] << " arr[2] : " << arr[2] << " arr.size() : " << arr.size() << std::endl;
-    std::copy(std::begin(crr), std::end(crr), append(brr));
-    std::cout << "brr[3] : " << brr[3] << " brr[1] : " << brr[1] << " brr[2] : " << brr[2] << " brr.size() : " << brr.size() << std::endl;
-    std::vector<int> xxx{7,18}, ddd;
-    size_t size = diffIndicesRange(ddd, brr, xxx);
-    std::cout << "size : " << size << std::endl;
-    std::cout << "ddd : {";
-    for (auto e : ddd) {
-        std::cout << e << ", ";
-    }
-    std::cout << "}\n";
+    result.clear();
+    return intersectIndices<RetCont, typename ARange::const_iterator, typename BRange::const_iterator,
+                            std::back_insert_iterator<RetCont>>
+      (result, begin(acont), end(acont), begin(bcont), end(bcont));
 }
 
+#include <gtest/gtest.h>
+
+TEST(SetOps, FixedSizeContainer)
+{
+    std::array<int, 3> a{1, 2, 3}, b{3, 4, 5}, c;
+    size_t size = diffIndices(c, begin(a), end(a), begin(b), end(b));
+    ASSERT_EQ(size, 2);
+    ASSERT_EQ(c[0], 1);
+    ASSERT_EQ(c[1], 2);
+    size = intersectIndices(c, begin(a), end(a), begin(b), end(b));
+    ASSERT_EQ(size, 1);
+    ASSERT_EQ(c[0], 3);
+}
+
+TEST(SetOps, VariableSizeContainer)
+{
+    std::vector<int> a{1, 2, 3}, b{3, 4, 5}, c;
+    size_t size = diffIndicesRange(c, a, b);
+    ASSERT_EQ(size, 2);
+    ASSERT_EQ(c[0], 1);
+    ASSERT_EQ(c[1], 2);
+    size = intersectIndicesRange(c, a, b);
+    ASSERT_EQ(size, 1);
+    ASSERT_EQ(c[0], 3);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
