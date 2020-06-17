4677a4b30346252548f3e46b3fae695fe836b379
Alexander Samoilov <alexander.samoilov@gmail.com>
2020-06-17 21:47:21 +0300

    added some methods to lambda closure sample.

--- misc/lambda_closure/frame_closure.cpp
+++ misc/lambda_closure/frame_closure.cpp
@@ -10,10 +10,17 @@ public:
     using init_function_t = std::function<void(const std::vector<uint8_t*>&)>;
     using draw_function_t = std::function<void(const State&, const std::vector<uint8_t*>&)>;
 
-    FrameReplay(uint32_t frameId, init_function_t initFunc, draw_function_t drawFunc)
-        : frameId_(frameId), initFunc_(initFunc), drawFunc_(drawFunc) {}
+    FrameReplay(uint32_t frameId,
+                init_function_t initFunc = [](const std::vector<uint8_t*> &){},
+                draw_function_t drawFunc = [](const State&, const std::vector<uint8_t*> &){})
+        : frameId_(frameId),
+          initFunc_(initFunc),
+          drawFunc_(drawFunc) 
+    {}
 
     uint32_t getFrameId() const { return frameId_; }
+    void setInitFunc(init_function_t initFunc) { initFunc_ = initFunc; }
+    void setDrawFunc(draw_function_t drawFunc) { drawFunc_ = drawFunc; }
     void init(const std::vector<uint8_t*> &buffer) { initFunc_(buffer); }
     void draw(const State &engine, const std::vector<uint8_t*> &buffer) { drawFunc_(engine, buffer); }
 
@@ -36,7 +43,7 @@ int main()
                                         {
                                             std::cout << "res77: " << res77 << " ";
                                             std::cout << "arr_0: " << arr_0 << std::endl;
-                                        }  );
+                                        } );
         frames.push_back(frame);
     }
     {
@@ -44,12 +51,13 @@ int main()
         // some resources
         static int res77;
         static const char arr_0[] = "frame #1 vs";
-        auto frame = new FrameReplay(1, [&](const std::vector<uint8_t*> &) { res77 = 2; },
-                                        [&](const State&, const std::vector<uint8_t*> &)
+        auto frame = new FrameReplay(1);
+        frame->setInitFunc([&](const std::vector<uint8_t*> &) { res77 = 2; });
+        frame->setDrawFunc([&](const State&, const std::vector<uint8_t*> &)
                                         {
                                             std::cout << "res77: " << res77 << " ";
                                             std::cout << "arr_0: " << arr_0 << std::endl;
-                                        }  );
+                                        } );
         frames.push_back(frame);
     }
 
