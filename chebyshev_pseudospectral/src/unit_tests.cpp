11920e1e47f630878062a4f4ca6a7bbdf7b4ff0f
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-02-20 17:12:04 +0000

    added the 2nd unit test for cftSuite.

--- chebyshev_pseudospectral/src/unit_tests.cpp
+++ chebyshev_pseudospectral/src/unit_tests.cpp
@@ -28,8 +28,27 @@
 TEST(cftSuite, test_cosfft1)
 {
     RowVectorXd x(3), x_inv(3);
+
     x     << 1.,  2., 3.;
     x_inv << 6., -2., 2.;
+
     cosfft1(x, 2);
+
     EXPECT_DOUBLE_EQ((x - x_inv).norm(), 0.0);
+}
+
+TEST(cftSuite, test_cf2)
+{
+    RowMatrixXd m(3, 3), m_inv(3, 3);
+
+    m << 1., 2., 3.,
+         4., 5., 6.,
+         7., 8., 9.;
+    m_inv << 6.,  4., 10.,
+            18., 10., 22.,
+            30., 16., 34.;
+
+    cft2(m, 2);
+
+    EXPECT_DOUBLE_EQ((m - m_inv).norm(), 0.0);
 }
