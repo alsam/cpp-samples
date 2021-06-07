8f42318a7d73e6fa815dc84fee8b290cfb420422
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-06-07 12:39:27 +0300

    stolen from amgcl

--- amgcl.greenfield/src/amgcl_block_matrices_test.cpp
+++ amgcl.greenfield/src/amgcl_block_matrices_test.cpp
@@ -1,8 +1,46 @@
 #include <amgcl/io/mm.hpp>
 #include <amgcl/adapter/crs_tuple.hpp>
 #include <amgcl/amg.hpp>
+#include <boost/program_options.hpp>
+// #include <format> // not yet supported
 
 int main(int argc, char *argv[])
 {
+    namespace po = boost::program_options;
+    namespace io = amgcl::io;
+
+    using std::vector;
+    using std::string;
+
+    po::options_description desc("Options");
+
+    desc.add_options()
+        ("help,h", "Show this help.")
+        ("prm-file,P",
+         po::value<string>(),
+         "Parameter file in json format. "
+        )
+        (
+         "prm,p",
+         po::value< vector<string> >()->multitoken(),
+         "Parameters specified as name=value pairs. "
+         "May be provided multiple times. Examples:\n"
+         "  -p solver.tol=1e-3\n"
+         "  -p precond.coarse_enough=300"
+        )
+        ("matrix,A",
+         po::value<string>(),
+         "System matrix in the MatrixMarket format. "
+         "When not specified, solves a Poisson problem in 3D unit cube. "
+        )
+        (
+         "block-size,b",
+         po::value<int>()->default_value(1),
+         "The block size of the system matrix. "
+         "When specified, the system matrix is assumed to have block-wise structure. "
+         "This usually is the case for problems in elasticity, structural mechanics, "
+         "for coupled systems of PDE (such as Navier-Stokes equations), etc. "
+        )
+        ;
 
 }
