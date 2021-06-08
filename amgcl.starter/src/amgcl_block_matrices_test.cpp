8a2c7899af6a3434d52f110914ff7c6ea076ffe2
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-06-08 14:41:16 +0300

    WIP for amgcl.starter

--- amgcl.starter/src/amgcl_block_matrices_test.cpp
+++ amgcl.starter/src/amgcl_block_matrices_test.cpp
@@ -2,6 +2,8 @@
 #include <amgcl/adapter/crs_tuple.hpp>
 #include <amgcl/amg.hpp>
 #include <boost/program_options.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 // #include <format> // not yet supported
 
 int main(int argc, char *argv[])
@@ -42,5 +44,44 @@ int main(int argc, char *argv[])
          "for coupled systems of PDE (such as Navier-Stokes equations), etc. "
         )
         ;
+
+
+    po::positional_options_description p;
+    p.add("prm", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+
+    for (int i = 0; i < argc; ++i) {
+        if (i) std::cout << " ";
+        std::cout << argv[i];
+    }
+    std::cout << std::endl;
+
+    boost::property_tree::ptree prm;
+    if (vm.count("prm-file")) {
+        read_json(vm["prm-file"].as<string>(), prm);
+    }
+
+    if (vm.count("prm")) {
+        for(const string &v : vm["prm"].as<vector<string> >()) {
+            amgcl::put(prm, v);
+        }
+    }
+
+    vector<ptrdiff_t> ptr, col;
+    vector<double> val, rhs, null, x;
+
+    if (vm.count("matrix")) {
+        string Afile  = vm["matrix"].as<string>();
+        auto [rows, cols] = io::mm_reader(Afile)(ptr, col, val);
+
+    }
 
 }
