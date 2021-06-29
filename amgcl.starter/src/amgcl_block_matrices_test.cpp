f0b522af7c43071cdd5b44f7114428d7aa995e70
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-06-29 11:26:40 +0300

    refactor scalar case to `scalar_solve`

--- amgcl.starter/src/amgcl_block_matrices_test.cpp
+++ amgcl.starter/src/amgcl_block_matrices_test.cpp
@@ -36,6 +36,141 @@
 
 // ./amgcl-block-matrices -A ../data/B_test.mtx  -b 3
 
+#include <amgcl/profiler.hpp>
+
+
+namespace amgcl { profiler<> prof; }
+using amgcl::prof;
+using amgcl::precondition;
+
+//---------------------------------------------------------------------------
+std::tuple<size_t, double> scalar_solve(
+        const boost::property_tree::ptree &prm,
+        size_t rows,
+        std::vector<ptrdiff_t> const &ptr,
+        std::vector<ptrdiff_t> const &col,
+        std::vector<double>    const &val,
+        std::vector<double>    const &rhs,
+        std::vector<double>          &x,
+        bool reorder
+        )
+{
+    Backend::params bprm;
+    // We use the tuple of CRS arrays to represent the system matrix.
+    // Note that std::tie creates a tuple of references, so no data is actually
+    // copied here:
+    auto A = std::tie(rows, ptr, col, val);
+
+#if defined(SOLVER_BACKEND_VEXCL)
+    vex::Context ctx(vex::Filter::Env);
+    std::cout << ctx << std::endl;
+    bprm.q = ctx;
+#elif defined(SOLVER_BACKEND_VIENNACL)
+    std::cout
+        << viennacl::ocl::current_device().name()
+        << " (" << viennacl::ocl::current_device().vendor() << ")\n\n";
+#elif defined(SOLVER_BACKEND_CUDA)
+    cusparseCreate(&bprm.cusparse_handle);
+    {
+        int dev;
+        cudaGetDevice(&dev);
+
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, dev);
+        std::cout << prop.name << std::endl << std::endl;
+    }
+#endif
+
+    using Solver1 = amgcl::make_solver<
+    amgcl::runtime::preconditioner<Backend>,
+    amgcl::runtime::solver::wrapper<Backend>>;
+
+    //using Solver2 = amgcl::make_block_solver<
+    using Solver2 = amgcl::make_solver<
+    // Use AMG as preconditioner:
+    amgcl::amg<
+        Backend,
+        amgcl::coarsening::smoothed_aggregation,
+        amgcl::relaxation::spai0
+        >,
+    // And BiCGStab as iterative solver:
+    amgcl::solver::bicgstab<Backend>>;
+
+    using Solver3 = amgcl::make_solver<
+    // Use AMG as preconditioner:
+    amgcl::amg<
+        Backend,
+        amgcl::coarsening::ruge_stuben,
+        amgcl::relaxation::spai0
+        //amgcl::relaxation::ilu0
+        >,
+    // And GMRES as iterative solver:
+    amgcl::solver::gmres<Backend>>;
+
+    try {
+        Solver2::params prm;
+        Solver2::backend_params bprm;
+        // prm.precond.direct_coarse = false;
+        prm.precond.direct_coarse = true;
+        prm.solver.maxiter = 1200;
+        prm.solver.verbose = true;
+        //prm.precond.coarsening ...
+
+#if defined(SOLVER_BACKEND_VEXCL)
+        vex::Context ctx(vex::Filter::Env);
+        std::cout << ctx << std::endl;
+        bprm.q = ctx;
+#elif defined(SOLVER_BACKEND_VIENNACL)
+        std::cout
+            << viennacl::ocl::current_device().name()
+            << " (" << viennacl::ocl::current_device().vendor() << ")\n\n";
+#elif defined(SOLVER_BACKEND_CUDA)
+        cusparseCreate(&bprm.cusparse_handle);
+        {
+            int dev;
+            cudaGetDevice(&dev);
+
+            cudaDeviceProp prop;
+            cudaGetDeviceProperties(&prop, dev);
+            std::cout << prop.name << std::endl << std::endl;
+        }
+#endif
+
+        Solver2 solve( A, prm, bprm );
+
+
+        auto f_b = Backend::copy_vector(rhs, bprm);
+        auto x_b = Backend::copy_vector(x,   bprm);
+
+        //auto [iters, error] = solve(A, rhs, x);
+        auto [iters, error] = solve(A, *f_b, *x_b);
+
+#if defined(SOLVER_BACKEND_VEXCL)
+        vex::copy(*x_b, x);
+#elif defined(SOLVER_BACKEND_VIENNACL)
+        viennacl::fast_copy(*x_b, x);
+#elif defined(SOLVER_BACKEND_CUDA)
+        thrust::copy(x_b->begin(), x_b->end(), x.begin());
+#else
+        std::copy(&(*x_b)[0], &(*x_b)[0] + rows, &x[0]);
+#endif
+
+        // Output the number of iterations, the relative error:
+        std::cout << "Iters: " << iters << std::endl
+                  << "Error: " << error << std::endl;
+        // std::cout << "x = [ ";
+        // for (auto el : x) {
+        //     std::cout << el << ", ";
+        // }
+        // std::cout << "]\n";
+        return {iters, error};
+    } catch(std::runtime_error &e) {
+        std::cout << "caught exception: " << e.what() << std::endl;
+    }
+
+    return {0, 0.0};
+}
+
 int main(int argc, char *argv[])
 {
     //namespace po = boost::program_options;
@@ -113,92 +248,7 @@ int main(int argc, char *argv[])
     }
 
     if (block_size == 1) { // scalar case
-
-        using Solver1 = amgcl::make_solver<
-        amgcl::runtime::preconditioner<Backend>,
-        amgcl::runtime::solver::wrapper<Backend>>;
-
-        //using Solver2 = amgcl::make_block_solver<
-        using Solver2 = amgcl::make_solver<
-        // Use AMG as preconditioner:
-        amgcl::amg<
-            Backend,
-            amgcl::coarsening::smoothed_aggregation,
-            amgcl::relaxation::spai0
-            >,
-        // And BiCGStab as iterative solver:
-        amgcl::solver::bicgstab<Backend>>;
-
-        using Solver3 = amgcl::make_solver<
-        // Use AMG as preconditioner:
-        amgcl::amg<
-            Backend,
-            amgcl::coarsening::ruge_stuben,
-            amgcl::relaxation::spai0
-            //amgcl::relaxation::ilu0
-            >,
-        // And GMRES as iterative solver:
-        amgcl::solver::gmres<Backend>>;
-
-        try {
-            Solver2::params prm;
-            Solver2::backend_params bprm;
-            // prm.precond.direct_coarse = false;
-            prm.precond.direct_coarse = true;
-            prm.solver.maxiter = 1200;
-            prm.solver.verbose = true;
-            //prm.precond.coarsening ...
-
-#if defined(SOLVER_BACKEND_VEXCL)
-            vex::Context ctx(vex::Filter::Env);
-            std::cout << ctx << std::endl;
-            bprm.q = ctx;
-#elif defined(SOLVER_BACKEND_VIENNACL)
-            std::cout
-                << viennacl::ocl::current_device().name()
-                << " (" << viennacl::ocl::current_device().vendor() << ")\n\n";
-#elif defined(SOLVER_BACKEND_CUDA)
-            cusparseCreate(&bprm.cusparse_handle);
-            {
-                int dev;
-                cudaGetDevice(&dev);
-
-                cudaDeviceProp prop;
-                cudaGetDeviceProperties(&prop, dev);
-                std::cout << prop.name << std::endl << std::endl;
-            }
-#endif
-
-            Solver2 solve( A, prm, bprm );
-
-
-            auto f_b = Backend::copy_vector(rhs, bprm);
-            auto x_b = Backend::copy_vector(x,   bprm);
-
-            //auto [iters, error] = solve(A, rhs, x);
-            auto [iters, error] = solve(A, *f_b, *x_b);
-
-#if defined(SOLVER_BACKEND_VEXCL)
-            vex::copy(*x_b, x);
-#elif defined(SOLVER_BACKEND_VIENNACL)
-            viennacl::fast_copy(*x_b, x);
-#elif defined(SOLVER_BACKEND_CUDA)
-            thrust::copy(x_b->begin(), x_b->end(), x.begin());
-#else
-            std::copy(&(*x_b)[0], &(*x_b)[0] + n, &x[0]);
-#endif
-
-            // Output the number of iterations, the relative error:
-            std::cout << "Iters: " << iters << std::endl
-                      << "Error: " << error << std::endl;
-            // std::cout << "x = [ ";
-            // for (auto el : x) {
-            //     std::cout << el << ", ";
-            // }
-            // std::cout << "]\n";
-        } catch(std::runtime_error &e) {
-            std::cout << "caught exception: " << e.what() << std::endl;
-        }
+        scalar_solve(prm, n, ptr,col, val, rhs, x, false);
     } else {
         // Compose the solver type
         using dmat_type = amgcl::static_matrix<double, 3, 3>;
