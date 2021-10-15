852a2d2a3c9f388e99c7d070aad38f62e0923a15
Alexander Samoilov <alexander.samoilov@gmail.com>
2021-10-15 13:54:58 +0300

    added simple2.cpp

---
+++ oneapi/simple2.cpp
@@ -0,0 +1,35 @@
+#include <CL/sycl.hpp>
+#include <array>
+#include <iostream>
+
+// dpcpp simple2.cpp -o simple2
+
+using namespace sycl;
+
+int main() {
+
+  constexpr int size=16;
+  std::array<int, size> data;
+
+  // Create queue on implementation-chosen default device
+  queue Q;
+
+  // Create buffer using host allocated "data" array
+  buffer B { data };
+
+  Q.submit([&](handler& h) {
+    accessor A{B, h};
+
+    h.parallel_for(size , [=](auto& idx) {
+	A[idx] = idx;
+    });
+  });
+
+  // Obtain access to buffer on the host
+  // Will wait for device kernel to execute to generate data
+  host_accessor A{B};
+  for (int i = 0; i < size; i++)
+    std::cout << "data[" << i << "] = " << A[i] << "\n";
+
+  return 0;
+}
